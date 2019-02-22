1d896f58b64210688d590be904906354c0db089c
patrick <patrick@patrickdeMacBook-Pro.local>
2019-02-22 11:17:10 -0800

    initialize soleMate_app

---
+++ soleMate_app/soleMate_app/soleMate_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soleMate_app
+//
+//  Created by patrick on 2019/2/22.
+//  Copyright © 2019 soleMate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
