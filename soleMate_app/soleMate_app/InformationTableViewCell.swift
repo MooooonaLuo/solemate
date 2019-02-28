ab81ae5fbd1e14dcd06b1a265842627f560daac2
Jiayu Luo <jil728@ucsd.edu>
2019-02-27 16:10:31 -0800

    update

---
+++ soleMate_app/soleMate_app/InformationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InformationTableViewCell.swift
+//  Solemate_app
+//
+//  Created by mona on 27/02/2019.
+//  Copyright © 2019 soleMate. All rights reserved.
+//
+
+import UIKit
+
+class InformationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
